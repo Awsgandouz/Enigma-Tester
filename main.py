4b0241766f41f153f9e1d2bf005c05d8029b579c
Aws ben gandouz <94719195+Awsgandouz@users.noreply.github.com>
2022-08-11 22:58:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from cProfile import label
 from cgitb import text
 from fileinput import filename
+
+
 from tkinter import *
-import os
+import os 
 
 os.system("clear")
 
@@ -12,18 +14,14 @@ root = Tk()
 root.title("Enigma~Testing")
 root.geometry('900x900') 
 
-
 #---->get the size of the file 
 def size():
     file_name = myTextbox.get()
     size_of_file = os.path.getsize(file_name)
     bashCommand = "openssl speed -bytes " + str(size_of_file) + "  aes-128-cbc aes-192-cbc aes-256-cbc 2> open "
     os.system(bashCommand) 
-    os.system("cut -d' ' -f2,10,14 open >output")
-    
-    
-
-    
+    os.system("cut -d' ' -f2,10 open >output.txt")
+    os.system("python3 draw.py")
 
 #---->solve app ui
 def solve(): 
@@ -34,22 +32,30 @@ def solve():
     main_label = Label(root,font = ("Helvetica" , 15),text="The size of the current file is "+str(size_of_file) + " bytes")
     main_label.pack()
 
+    hello_label = Label(root,text="Dashboard:")
+    hello_label.pack()
+    
+
     button = Button (root, text="Exit", font = ("Helvetica" , 15) ,command=root.destroy)
     button.pack()
- 
 
+
+mylabel = Label(root , text="Enter file name! ")
 
 mylabel = Label(root , text="Enter file name! " , font = ("Helvetica" , 15))
 mylabel.pack()
 
+myTextbox = Entry(root , width=30)
 myTextbox = Entry(root , width=40)
 myTextbox.pack()
 
+myButton = Button(root , text="Test the speed!" , command=solve)
 myButton = Button(root , text="Test the speed!" ,font = ("Helvetica" , 15) ,command=solve)
 myButton.pack()
 
 
 
+root.mainloop()
 
 
 root.mainloop
