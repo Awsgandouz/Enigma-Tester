3e60d1391182ea1418fc9525321c1c55f0d00e67
Aws ben gandouz <94719195+Awsgandouz@users.noreply.github.com>
2022-08-16 09:39:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,55 +1,72 @@
 from cProfile import label
 from cgitb import text
 from fileinput import filename
-
-
+from cProfile import label
+from cgitb import text
+from fileinput import filename
+import os
 from tkinter import *
-import os 
-
-os.system("clear")
-
-
+from PIL import Image, ImageTk
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+  
 # Tkinter GUI
+os.system("clear")
 root = Tk() 
 root.title("Enigma~Testing")
 root.geometry('900x900') 
 
-#---->get the size of the file 
-def size():
+def plot():
     file_name = myTextbox.get()
+    # get the size of the file
     size_of_file = os.path.getsize(file_name)
     bashCommand = "openssl speed -bytes " + str(size_of_file) + "  aes-128-cbc aes-192-cbc aes-256-cbc 2> open "
     os.system(bashCommand) 
     os.system("cut -d' ' -f2,10 open >output.txt")
-    os.system("python3 draw.py")
+    
+    # the figure that will contain the plot
+    fig = Figure(figsize = (5, 5),dpi = 100)
+    algo = []
+    nb = []
+    f = open('output.txt','r')
+    for row in f:
+        row = row.split(' ')
+        algo.append(row[0])
+        nb.append(int(row[1]))  
+    
+    plot1 = fig.add_subplot()
+    # plotting the graph
+    plot1.bar(algo, nb, color = 'g', label = 'File Data')
+    # creating the Tkinter canvas
+    # containing the Matplotlib figure
+    canvas = FigureCanvasTkAgg(fig,master = root)  
+    canvas.draw()
+    # placing the canvas on the Tkinter window
+    canvas.get_tk_widget().pack()
+    # creating the Matplotlib toolbar
+    toolbar = NavigationToolbar2Tk(canvas,root)
+    toolbar.update()
+    # placing the toolbar on the Tkinter window
+    canvas.get_tk_widget().pack()
+
 
 #---->solve app ui
-def solve(): 
-    size()
+def solve():
     file_name = myTextbox.get()
     size_of_file = os.path.getsize(file_name)
-
-    main_label = Label(root,font = ("Helvetica" , 15),text="The size of the current file is "+str(size_of_file) + " bytes")
-    main_label.pack()
-
-    hello_label = Label(root,text="Dashboard:")
-    hello_label.pack()
-    
-
+    label1 = Label(root,font = ("Helvetica" , 15),text="The size of the current file is "+str(size_of_file) + " bytes")
+    label1.pack()
+    plot()
     button = Button (root, text="Exit", font = ("Helvetica" , 15) ,command=root.destroy)
     button.pack()
 
 
-mylabel = Label(root , text="Enter file name! ")
+label2 = Label(root , text="Enter file name! " , font = ("Helvetica" , 15))
+label2.pack()
 
-mylabel = Label(root , text="Enter file name! " , font = ("Helvetica" , 15))
-mylabel.pack()
-
-myTextbox = Entry(root , width=30)
 myTextbox = Entry(root , width=40)
 myTextbox.pack()
 
-myButton = Button(root , text="Test the speed!" , command=solve)
 myButton = Button(root , text="Test the speed!" ,font = ("Helvetica" , 15) ,command=solve)
 myButton.pack()
 
@@ -57,5 +74,3 @@ myButton.pack()
 
 root.mainloop()
 
-
-root.mainloop
