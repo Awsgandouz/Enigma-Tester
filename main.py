eca2d630d73be776912d8bdbd3b7850d0e38de98
Aymen-Besbes <109146415+Aymen-Besbes@users.noreply.github.com>
2022-09-05 21:36:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,25 +31,48 @@ class App(customtkinter.CTk):
 
         self.frame_left = customtkinter.CTkFrame(master=self,width=180,corner_radius=0)
         self.frame_left.grid(row=0, column=0, sticky="nswe")
-
+        self.frame_center = customtkinter.CTkFrame(master=self)
+        self.frame_center.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
         
-        self.frame_right = customtkinter.CTkFrame(master=self)
-        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
         
-        # ============ frame_left ============
-
+        
+        # ============ Description ===========
+        
+        self.frame_info = customtkinter.CTkFrame(self.frame_center)
+        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
+        self.frame_info.rowconfigure(0, weight=1)
+        self.frame_info.columnconfigure(0, weight=1)
+        self.label_info_1 = customtkinter.CTkLabel(master=self.frame_info,
+                                                   text="Enigma tester is an application \n" +
+                                                        "which is used to calculate the execution ,\n" +
+                                                        "time of encryption algorithms " ,
+                                                   height=20,
+                                                   corner_radius=6,  # <- custom corner radius
+                                                   fg_color=("white", "#363636"),  # <- custom tuple-color
+                                                   justify=tkinter.LEFT)
+        self.label_info_1.grid(column=0, row=0, sticky="nwse", padx=15, pady=15)
+          
+        
+        ####
+        self.frame_center.rowconfigure((0, 1, 2, 3), weight=1)
+        self.frame_center.rowconfigure(7, weight=10)
+        self.frame_center.columnconfigure((0, 1), weight=1)
+        self.frame_center.columnconfigure(2, weight=0)
         # configure grid layout (1x11)
         self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
         self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
-
+    
         self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
                                               text="Menu",
                                               text_font=("Roboto Medium", -16),
                                             ) 
+                                        
         self.label_1.grid(row=1, column=0, pady=10, padx=10)
 
+        
+        
         self.description_image = self.load_image("/home/aws/Desktop/projet-ensi/description.jpg", 30)
 
         self.button_1 = customtkinter.CTkButton(master=self.frame_left,
@@ -58,7 +81,7 @@ class App(customtkinter.CTk):
                                                 image=self.description_image)
         self.button_1.grid(row=2, column=0, pady=10, padx=20)
 
-        self.test_image = self.load_image("/home/aws/Desktop/projet-ensi/test.jpg", 30)
+        self.test_image = self.load_image("/home/aws/Desktop/projet-ensi/test.jpgg", 30)
         self.button_2 = customtkinter.CTkButton(master=self.frame_left,
                                                 text="Testing",
                                                 command="",
@@ -77,29 +100,32 @@ class App(customtkinter.CTk):
         # ============ frame_right ============
         
         # configure grid layout (3x7)
-        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
-        self.frame_right.rowconfigure(7, weight=10)
-        self.frame_right.columnconfigure((0, 1), weight=1)
-        self.frame_right.columnconfigure(2, weight=0)
+      
 
         # ============ frame_right ============
+        Description_Text=customtkinter.CTkTextbox()
+        # set_dimensions(self, width=None, height=None):
+        # super().set_dimensions(width, height)
 
-        self.radio_var = tkinter.IntVar(value=0)
+        self.radio_var = tkinter.IntVar()
+        self.radio_var.set(-1)
         
-        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.frame_right,variable=self.radio_var,value=1,text="AES")
+        self.radio_button_1 = customtkinter.CTkRadioButton(master=self.frame_center,
+                                 variable=self.radio_var,value=1,text="AES")
         
         self.radio_button_1.grid(row=8, column=0, pady=5, padx=20, sticky="n")
         
-        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.frame_right,variable=self.radio_var,value=0 ,text="RSA")
+        self.radio_button_2 = customtkinter.CTkRadioButton(master=self.frame_center,
+                                 variable=self.radio_var,value=0 ,text="RSA")
         
         self.radio_button_2.grid(row=8, column=1, pady=5, padx=20, sticky="n")
 
-        self.entry = customtkinter.CTkEntry(master=self.frame_right,
+        self.entry = customtkinter.CTkEntry(master=self.frame_center,
                                             width=120,
                                             placeholder_text="select file")
         self.entry.grid(row=7, column=0, columnspan=2, pady=20, padx=20, sticky="we")
 
-        self.button_5 = customtkinter.CTkButton(master=self.frame_right,
+        self.button_5 = customtkinter.CTkButton(master=self.frame_center,
                                                 text="Start",
                                                 border_width=2,  
                                                 fg_color=None,
@@ -107,11 +133,8 @@ class App(customtkinter.CTk):
         
         self.button_5.grid(row=7, column=2, columnspan=1, pady=20, padx=20, sticky="we")
         self.optionmenu_1.set("Dark")    
-        self.radio_button_1.select()
         
         
-        
-    
     def load_image(self, path, image_size):
         return ImageTk.PhotoImage(Image.open(path).resize((image_size, image_size)))
 
@@ -135,3 +158,4 @@ class App(customtkinter.CTk):
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+    
